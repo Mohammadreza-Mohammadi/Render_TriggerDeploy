4175b29febbb44ee835260076626de5b69babc24
Mohammadreza Mohammadi <mreza.mdi@gmail.com>
2026-05-05 01:00:36 +0330

    feat add deploy js to trigger render using webhook

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const https = require("https");
+
+// اینو از Render Deploy Hook می‌گیری
+const DEPLOY_HOOK_URL =`https://api.render.com/deploy/srv-d7sgol8sfn5c73fgugg0?key=l7XYcFYfHAo`;
+
+if (!DEPLOY_HOOK_URL) {
+  console.error("❌ Missing RENDER_DEPLOY_HOOK env variable");
+  process.exit(1);
+}
+
+function deploy() {
+  console.log("🚀 Triggering deploy on Render...");
+
+  https
+    .get(DEPLOY_HOOK_URL, (res) => {
+      const { statusCode } = res;
+
+      if (statusCode >= 200 && statusCode < 300) {
+        console.log("✅ Deploy triggered successfully!");
+      } else {
+        console.log("❌ Deploy failed with status:", statusCode);
+      }
+    })
+    .on("error", (err) => {
+      console.error("❌ Error:", err.message);
+    });
+}
+
+deploy();
